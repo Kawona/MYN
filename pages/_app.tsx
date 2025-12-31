d18b1614d343921d11317640326541a03ca1e810
Kawona <ojekaona@gmail.com>
2025-12-31 12:09:45 +0000

    added logo added logo brand to browser tab

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import "../styles/global.css";
 
 import Header from "@/components/layout/Header";
@@ -7,6 +8,11 @@ import Footer from "@/components/layout/Footer";
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Meridian Youth Network</title>
+        <link rel="icon" href="/images/logo/mynLogo.jpg" />
+      </Head>
+
       <Header />
       <Component {...pageProps} />
       <Footer />
